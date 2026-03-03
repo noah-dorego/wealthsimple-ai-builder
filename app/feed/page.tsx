b9683e7ce9c6139223233615113920010d1bf9d6
Noah do Rego <ndore051@uottawa.ca>
2026-03-02 22:22:59 -0500

    add dynamic rerenders to make fresh calls to getDb()

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { getFeedSources, getFeedItems } from "@/lib/db";
 import { FeedInbox } from "@/components/FeedInbox";
 import { FeedSourceList } from "@/components/FeedSourceList";
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import { getStats, getFindings } from "@/lib/db";
 import { FindingFilters } from "@/components/FindingFilters";
